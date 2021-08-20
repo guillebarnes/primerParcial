c66abbc58eb2895d406a91357f73ebffd3d87306
guillebarnes <guillebarnes_95@hotmail.com>
2021-08-20 16:54:27 -0300

    agrego setters en la cancha

--- src/main/java/domain/entities/club/Cancha.java
+++ src/main/java/domain/entities/club/Cancha.java
@@ -7,6 +7,9 @@ public class Cancha {
     private boolean techada;
     private double precio;
 
+    public Cancha(){
+
+    }
     public Cancha(Boolean techada, double precio, int id_club) {
         this.techada = techada;
         this.precio = precio;
@@ -14,6 +17,18 @@ public class Cancha {
         CanchaMapper canchaMapper = new CanchaMapper(this.techada, this.precio, id_club);
         this.id = canchaMapper.insert();
 
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTechada(boolean techada) {
+        this.techada = techada;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
     }
 
     public int getId() { return this.id; }
